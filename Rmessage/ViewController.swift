10a20db616a5213a10cfb6bc4fb7f4cdc9d6bbac
Rafael Rios <Wukengch@gmail.com>
2019-09-04 10:11:20 -0500

    first commit in iOS basic messaging app

---
+++ Rmessage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rmessage
+//
+//  Created by rafael rios garcia on 9/4/19.
+//  Copyright © 2019 ARES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
